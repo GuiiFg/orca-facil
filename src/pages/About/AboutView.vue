38ffa18cbab9fe436f0d40857a12fb65e1e8c717
GuiiFg <farias._02f@outlook.com>
2025-10-24 08:54:07 -0300

    base orca facil

---
+++ src/pages/About/AboutView.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>About</h1>
+  <fwb-alert type="success">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</fwb-alert>
+</template>
+
+<script setup>
+import { FwbAlert } from 'flowbite-vue'
+</script>
